1bdbc7b215be38c7bba03a664d3574a07ec3b819
Igor <ikor1321@yandex.ru>
2023-08-07 14:52:18 +0300

    <style>:<Поправил текст>

--- src/main/java/ru/yandex/practicum/filmorate/controller/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ValidationException.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.nio.charset.CoderResult;
-
 @Slf4j
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Provided incorrect data")
 public class ValidationException extends RuntimeException {
