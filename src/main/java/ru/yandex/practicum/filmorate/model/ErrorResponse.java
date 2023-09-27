30758a9de87c86d96bbfda6d36d81749d519d692
Igor <ikor1321@yandex.ru>
2023-09-28 01:42:11 +0300

    <style>:<Убрал лишние импотры>

--- src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ErrorResponse.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 public class ErrorResponse {
     private String error;
 
-    public ErrorResponse(String error){
+    public ErrorResponse(String error) {
         this.error = error;
     }
 
